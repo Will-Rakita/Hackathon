4694dc3c8493b019e5694fe2641b4591ce31c6b4
eymnzx <emiz.yang@gmail.com>
2024-10-19 15:28:57 -0400

    fixed logout icon

--- client/src/layouts/Central/HeaderLinks/HeaderLinks.tsx
+++ client/src/layouts/Central/HeaderLinks/HeaderLinks.tsx
@@ -10,7 +10,7 @@ function HeaderLinks() {
       <Link to={"/"}>Return To Menu</Link>
       |<img src="https://cdn-icons-png.flaticon.com/512/25/25613.png" alt="map" /><Link to={"/sitemap"}>Site Map</Link>
       |<img src="https://icons.veryicon.com/png/o/miscellaneous/flat-icon/help-252.png" alt="help" /><Link to={"/help"}>Help</Link>
-      |<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg-6Z1CeO2FgHNrV_H7xRMbhTMnvK2xIgO7A&s" alt="login" /><Link to={"/login"}>
+      |<img src="https://cdn-icons-png.flaticon.com/512/126/126467.png" alt="login" /><Link to={"/login"}>
         <span onClick={() => logout()}>Logout</span>
       </Link>
     </div>
