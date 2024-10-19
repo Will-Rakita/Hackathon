1712d2181caf04a217076b7e169c0d2ee56cd8ba
Arcan3s <155115078+Arcan3s@users.noreply.github.com>
2024-10-19 14:01:16 -0400

    Add nyan-dog gif to 404 page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -5,6 +5,7 @@ function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
       <h1>404</h1>
+      <iframe src="https://giphy.com/embed/5E7vDOIamcWlzg97TG" width="480" height="480" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/wirdesign-dog-hotdog-nyan-5E7vDOIamcWlzg97TG">via GIPHY</a></p>
     </Layout>
   );
 }
