37be21185d85b43954e2d89ab1deae250f05c3e6
Will rakita <willrakita@cmail.carleton.ca>
2024-10-19 16:00:52 -0400

    Got rid of the weekend

--- client/src/components/Timetable/Timetable.tsx
+++ client/src/components/Timetable/Timetable.tsx
@@ -1,13 +1,11 @@
 import "./Timetable.style.scss";
 
 export enum Days {
-  Sunday = "Sunday",
   Monday = "Monday",
   Tuesday = "Tuesday",
   Wednesday = "Wednesday",
   Thursday = "Thursday",
   Friday = "Friday",
-  Saturday = "Saturday",
 }
 
 export interface CalendarBlock {
